48be501286483a41ac2cfaa3be25845f6d5d99c8
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-03-17 16:16:29 -0600

    Short script that parses sample descriptive data into files for VCF filtering.

---
+++ generate_keepfiles.r
@@ -0,0 +1,37 @@
+################################################################################################################
+# Short script for parsing the tick sample data into different "keep" files that can be used for VCF filtering #
+################################################################################################################
+
+## RUN ONLY ONCE ##
+# read in the data
+sample.data<-read.csv("~/Desktop/UT_ddRADseq/ddRAD_FinalLibrary_SampleInfo.csv")
+
+# reconstruct the combinatoric label used in the VCF file
+combo.label<-paste(sample.data$tick.id, '_', sample.data$Inline.Barcode, '-', sample.data$Illumina.Barcode, sep='')
+full.data<-cbind(sample.data, combo.label)
+write.csv(full.data, file='~/Desktop/UT_ddRADseq/ddRAD_FinalLibrary_SampleInfo_Full.csv', row.names=FALSE)
+
+## RUN AS NEEDED
+
+setwd('~/Desktop/UT_ddRADseq/')
+full.data<-read.csv('ddRAD_FinalLibrary_SampleInfo_Full.csv')
+
+# define a function that subsets the data
+keep.subset<-function(data, column.idx){
+  characteristics<-unique(data[column.idx])
+  for(i in characteristics[[1]]){
+    print(i)
+    keep.name=file.path(getwd(), paste('keep_', i, '.txt', sep=''))
+    keep.data=subset(data, data[column.idx]==i)$combo.label
+    write.table(keep.data, file=keep.name, row.names=FALSE, col.names=FALSE, quote=FALSE)
+  }
+}
+
+# separate Harrison Bayou and Starr Ranch Trail
+keep.subset(full.data, 6)
+
+# separate raccoon and opossum hosts
+keep.subset(full.data, 4)
+
+# separate Harrison Bayou north and south
+keep.subset(subset(full.data, coll.site=='HB'), 8)
