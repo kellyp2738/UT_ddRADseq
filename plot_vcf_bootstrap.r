bd603db6806ef2d048a65fe2205cb8300279acfa
kellyp2738 <kellyannepierc@gmail.com>
2015-04-30 17:48:57 -0600

    Revised statistical inference and plot aesthetics.

--- plot_vcf_bootstrap.r
+++ plot_vcf_bootstrap.r
@@ -4,79 +4,88 @@
 library(gplots)
 library(plyr)
 library(plotrix)
+library(stringr)
+library(scales)
 
 args<-commandArgs()
 
 infile<-args[1]
 
 # read in the snp data file
-data<-read.table(infile)
+data<-read.table(infile, header=FALSE)
 
-data<-read.csv('~/Desktop/snp_bootstrap_test.txt', header=FALSE)
+data<-read.csv('~/Desktop/snp_bootstrap_final.txt', header=FALSE)
 names(data)<-c('n', 'snps')
-boxplot(snps~n, data=data)
-plot(snps~n, data=data)
 
+# create a boxplot that also displays the raw data (jittered on x-axis)
+par(mar=c(5,5,2,2))
+boxplot(snps~n, data=data, axes=FALSE,
+        ylim=c(round_any(min(data$snps), 1000, f=floor), 
+               round_any(max(data$snps), 1000, f=ceiling)),
+        ylab='SNPs Recovered (1000s)', xlab='Sample Size', cex.lab=1.5)
+for(i in 1:((max(data$n)-min(data$n))+1)){
+  n.data<-subset(data, n==unique(data$n)[i])
+  points(jitter(rep(i, length(n.data$n)), amount=0.1), n.data$snps, pch=16, col=alpha('red', 0.25))
+}
+axis(side=1, cex.axis=1.5, at=1:length(unique(data$n)), labels=c(min(data$n):max(data$n)))
+axis(side=2, las=1, at=seq(round_any(min(data$snps), 1000, f=floor), 
+                           round_any(max(data$snps), 1000, f=floor)+1000, 1000),
+     labels=seq(round_any(min(data$snps), 1000, f=floor), 
+                round_any(max(data$snps), 1000, f=floor)+1000, 1000)/1000, cex.axis=1.5)
+
+# determine average snp recover
 ns<-c()
 means<-c()
 for(i in unique(data$n)){
   ns<-c(ns, i)
   means<-c(means, mean(subset(data, n==i)$snps))
 }
-  
-plot(ns, means)
 
-
-aov(model)
-confint(model)
-
-model2<-aov(snps~as.factor(n), data=data)
-summary(model2)
-
-model3<-glm(snps~n, data=data, family=poisson)
-summary(model3)
-anova(model3)
-
-ci.3<-confint(model3)
-lower.ci<-function(x){
-  y=x*ci.3[2,2]+coef(model3)[1]
-  return(y)
-}
-
-upper.ci<-function(x){
-  y=x*ci.3[2,1]+coef(model3)[1]
-  return(y)
-}
-
-fit<-function(x){
-  y=exp(x*coef(model3)[2])+coef(model3)[1]
-}
-
-plot(data$n, fit(data$n), type='l')
-lines(data$n, lower.ci(data$n), lty=2)
-lines(data$n, upper.ci(data$n), lty=2)
-
-fit3.with.ci<-predict(model3, data, level=0.95, interval="confidence")
+# logistic regression on count data
+log.model<-glm(snps~n, data=data, family=poisson)
+summary(log.model)
+plot(resid(log.model))
+fit.model<-predict(log.model, type=c("response"), se.fit=TRUE)
+plot(x=min(data$n):max(data$n), y=unique(fit.model$fit), type='l')
+lines(x=min(data$n):max(data$n), y=unique(fit.model$fit)-(unique(fit.model$se.fit)*1.96), lty=2)
+lines(x=min(data$n):max(data$n), y=unique(fit.model$fit)+(unique(fit.model$se.fit)*1.96), lty=2)
+# the standard error for the parameter estimate in this model is incredibly tiny
+# looking back at the distribution of the counts, the distributions seem normal
+mean(data$snps[1:100])
+sqrt(var(data$snps[1:100]))
+# mean != variance --> logistic regression with a poisson link function does not seem to fit this data set
+# linear regression on count data is okay when the counts are not near zero
+# in our case, our counts are so far from zero that we needn't worry about negative predicted values
+# and so... a linear regression model as an alternative
 
 # a simple linear regression model
 # not appropriate for count data, but the plot was reasonably difficult to make so its code are left intact
-model<-lm(snps~n, data=data)
-fit.with.ci<-predict(model, data, level=0.95, interval="confidence")
-rsq.label<-bquote(R^2~.('=')~.(round(summary(model)$r.squared, 2)))
+linear.model<-lm(snps~n, data=data)
+summary(linear.model)
+fit.with.ci<-predict(linear.model, data, level=0.95, interval="prediction", )
+rsq.label<-bquote(R^2~.('=')~.(round(summary(linear.model)$r.squared, 2)))
+pval.label<-bquote(p~.('=')~.(format(summary(linear.model)$coefficients[2,4], digits=2)))
 
+
+png(file='~/Desktop/test_plot.png', height=15, width=30, res=300, units='cm')
 par(mar=c(5,5,2,2))
 plot(x=data$n, y=fit.with.ci[,1], type="l", 
-     ylim=c(round_any(min(fit.with.ci[,2]), 100, f=floor), 
-            round_any(max(fit.with.ci[,3]), 100, f=floor)+1000), 
+     ylim=c(round_any(min(data$snps), 1000, f=floor), 
+            round_any(max(data$snps), 1000, f=ceiling)), 
      axes=FALSE, xlab='Sample Size', ylab='SNPs Recovered (1000s)',
      cex.lab=1.5, cex.axis=1.5)
 lines(x=data$n, y=fit.with.ci[,2], lty=2)
 lines(x=data$n, y=fit.with.ci[,3], lty=2)
+points(data$n, data$snps, pch=16, col=alpha('red', 0.25))
 axis(side=1, cex.axis=1.5)
-axis(side=2, las=1, at=seq(round_any(min(fit.with.ci[,2]), 1000, f=floor), 
-                           round_any(max(fit.with.ci[,3]), 1000, f=floor)+1000, 1000),
-     labels=seq(round_any(min(fit.with.ci[,2]), 1000, f=floor), 
-                round_any(max(fit.with.ci[,3]), 1000, f=floor)+1000, 1000)/1000, cex.axis=1.5)
-corner.label(label=rsq.label, x=1, y=1)
+axis(side=2, las=1, at=seq(round_any(min(data$snps), 1000, f=floor), 
+                           round_any(max(data$snps), 1000, f=ceiling), 1000),
+     labels=seq(round_any(min(data$snps), 1000, f=floor), 
+                round_any(max(data$snps), 1000, f=ceiling), 1000)/1000, cex.axis=1.5)
+corner.coords<-corner.label(x=1, y=1)
+text(corner.coords$x-2.5, corner.coords$y-2527, labels=pval.label, adj=0)
+text(corner.coords$x-2.5, corner.coords$y-3250, labels=rsq.label, adj=0)
+legend(x=corner.coords$x-2.72, y=corner.coords$y, legend=c('Fit', '95% CI'), lty=c(1, 2), bty='n')
+dev.off()
 
 
