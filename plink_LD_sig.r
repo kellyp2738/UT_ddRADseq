48e81209b777a240da33cc1d88653290ec52dc75
kellyp2738 <kellyannepierc@gmail.com>
2015-06-11 14:37:23 -0600

    Script to calculate significance of R2 for SNP pairs as an LD screen. Outputs the count of significant pairs to screen.

---
+++ plink_LD_sig.r
@@ -0,0 +1,19 @@
+# calculate significance of LD R^2 and make FDR correction
+
+library('fdrtools')
+
+options <- commandArgs()
+
+infile <- options[1]
+
+ld <- read.table(infile, header=TRUE)
+
+N <- 22 # number chromosomes in D. variabilis
+
+ld$chisq <- ld$R2*N
+ld$p <- dchisq(ld$chisq, df=1) # 2 alleles, df = 1
+ld$q <- fdrtool(ld$p, statistic=c('pvalue'))
+sig.ld <- subset(ld, q < 0.05, R2 > 0.8)
+num.sig <- length(sig.ld[,1])
+
+print(num.sig) # this should end up putting the number of sig. LD pairs in std. error output that Python can parse
