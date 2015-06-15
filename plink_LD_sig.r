76547f889b577d4f507cb9a0c81c1b2a9d49aae1
kellyp2738 <kellyannepierc@gmail.com>
2015-06-15 16:08:23 -0600

    Fixed bug in subsetting by qvalue and R2.

--- plink_LD_sig.r
+++ plink_LD_sig.r
@@ -5,19 +5,21 @@ library('fdrtool')
 options <- commandArgs(TRUE)
 
 infile <- options[1]
+#print(infile)
 
 ld <- read.table(infile, header=TRUE)
-ld<-read.table('~/Desktop/UT_ddRADseq/plink_edit.ld', header=TRUE)
+#ld<-read.table('~/Desktop/UT_ddRADseq/plink_edit-2.ld', header=TRUE)
 
 N <- 22 # number chromosomes in D. variabilis
 
 ld$chisq <- ld$R2*N
 ld$p <- dchisq(ld$chisq, df=1) # 2 alleles, df = 1
-print(length(ld$p))
+#print(length(ld$p))
 ld.fdr <- fdrtool(ld$p, statistic=c('pvalue'), plot=FALSE)
-sig.ld <- subset(ld, q < 0.05, R2 > 0.8)
+ld$q <- ld.fdr$qval
+sig.ld <- subset(ld, q < 0.05 & R2 > 0.8)
 num.sig <- length(sig.ld[,1])
 
-print(num.sig) # this should end up putting the number of sig. LD pairs in std. error output that Python can parse
+print(c(length(ld$p), num.sig)) # this should end up putting the number of sig. LD pairs in std. error output that Python can parse
 
 ## WHY DO I ONLY GET 84 PAIRWISE RESULTS FOR LD FROM PLINK??
