5a9fe0b08646c972b6eeded741a0e21df378c05f
Kelly Anne Pierce <kellyannepierce@gmail.com>
2015-06-15 10:33:13 -0600

    Added comments to pre-existing code.

--- plink_LD_sig.r
+++ plink_LD_sig.r
@@ -7,13 +7,17 @@ options <- commandArgs(TRUE)
 infile <- options[1]
 
 ld <- read.table(infile, header=TRUE)
+ld<-read.table('~/Desktop/UT_ddRADseq/plink_edit.ld', header=TRUE)
 
 N <- 22 # number chromosomes in D. variabilis
 
 ld$chisq <- ld$R2*N
 ld$p <- dchisq(ld$chisq, df=1) # 2 alleles, df = 1
-ld$q <- fdrtool(ld$p, statistic=c('pvalue'))
+print(length(ld$p))
+ld.fdr <- fdrtool(ld$p, statistic=c('pvalue'), plot=FALSE)
 sig.ld <- subset(ld, q < 0.05, R2 > 0.8)
 num.sig <- length(sig.ld[,1])
 
 print(num.sig) # this should end up putting the number of sig. LD pairs in std. error output that Python can parse
+
+## WHY DO I ONLY GET 84 PAIRWISE RESULTS FOR LD FROM PLINK??
