eed57a715aa93de770a7a2bc409c3c51c2e77816
kellyp2738 <kellyannepierc@gmail.com>
2015-06-15 16:09:16 -0600

    Added subprocess calls to Plink and R. In progress: capturing R output to stdout, stderr.

--- sample_IDs.py
+++ sample_IDs.py
@@ -72,17 +72,24 @@ snp_line = split_stderr[15]
 snp_count = [int(s) for s in snp_line.split() if s.isdigit()][0] # split snp_line, search for integers, and save the first one found
 
 # convert the VCF file to a PLINK file
-q = Popen(["vcftools", "--vcf", "/home/antolinlab/Desktop/vcf_tmp.recode.vcf", "--plink", "--out", "/home/antolinlab/Desktop/vcf_tmp_plink"])
+subprocess.call("vcftools --vcf /home/antolinlab/Desktop/vcf_tmp.recode.vcf --plink --out /home/antolinlab/Desktop/vcf_tmp_plink", shell=True)
 
-plink --file vcf_tmp_plink --matrix
-plink --file vcf_tmp_pling --inter-chr
+# calculate LD on the plink file
+# subprocess.call("plink --file vcf_tmp_plink --matrix", shell=True) # option A: pairwise matrix
+#subprocess.call("plink --file /home/antolinlab/Desktop/vcf_tmp_plink --ld --inter-chr --allow-no-sex --noweb", shell=True) # option B: long-format list
+subprocess.call("plink --file /home/antolinlab/Desktop/vcf_tmp_plink --r2 --inter-chr --noweb", shell=True) # option B: long-format list
 
 # PLINK files have extra spaces to make them human readable... remove those spaces for R import
-sed -E 's|  *| |g' plink.ld > plink_edit.ld
-r = Popen(["Rscript", "plink_edit.ld"])
+subprocess.call("sed -E 's|  *| |g' plink.ld > plink_edit-2.ld", shell=True)
+r = Popen(["Rscript", "plink_LD_sig.r", "plink_edit-2.ld"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+outs, errs = r.communicate()
+#split_stderr_r = str.splitlines(stderr_r)
+print outs, errs
+snp_pairs = split_stderr_r[1]
+sig_pairs = split_stderr_r[2]
 
 # save the sample size and snp count
-write_line = [str(n), ",", str(snp_count), "\n"]
+write_line = [str(n), ",", str(snp_count), ",", str(snp_pairs), ",", str(sig_pairs), "\n"]
 final_data.writelines(write_line)
 final_data.close()
 
