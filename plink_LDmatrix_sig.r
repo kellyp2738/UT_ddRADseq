e7a423ee1daed769f0b8c737d7c7be4853854047
kellyp2738 <kellyannepierc@gmail.com>
2015-06-18 15:43:07 -0600

    In progress: parallel processing of large plink dataset.

--- plink_LDmatrix_sig.r
+++ plink_LDmatrix_sig.r
@@ -1,32 +1,38 @@
 ## calculate significance of LD R^2 and make FDR correction (matrix input)
 
+library(doMC)
+library(bigtabulate)
+
 options <- commandArgs(TRUE)
 
 infile <- options[1]
 
 N <- 22 # number chromosomes in D. variabilis
 
+# read data
 ld.matrix <- read.table(infile)
-ld.matrix <- read.table("~/Desktop/UT_ddRADseq/plink.ld", na.strings = "nan")
-matrix.size <- dim(ld.matrix[1])
+ld.matrix <- read.big.matrix("~/Desktop/UT_ddRADseq/plink.ld", sep=" ", type='double')#, na.strings = "nan")
 
-# calculate chi sq. p-values
-chisq.mat <- matrix(data=NA, nrow=matrix.size, ncol=matrix.size)
-chisq.mat[upper.tri(chisq.mat)] <- dchisq((ld.matrix[upper.tri(ld.matrix)]*N), 1) # 2 alleles, df = 1
+# reduce data size
+ld.vector <- ld.matrix[upper.tri(ld.matrix)] # change matrix to a vector to reduce its size
+rm(ld.matrix) # clear large matrix out
 
-# FDR using p.adjust() in {base}
-ld.fdr <- p.adjust(chisq.mat[upper.tri(chisq.mat)], method=c('fdr'))
+# parallelize the operations on the vector
+registerDoMC(cores=4)
+idx <- bigsplit(ld.vector)
+# calculate p-value
+ld.p.value <- dchisq((ld.vector*N), 1)
 
-results.df <- data.frame(cbind(ld.matrix[upper.tri(ld.matrix)], ld.fdr))
+# calculate q-value
+ld.fdr <- p.adjust(ld.p.value, method=c('fdr'))
+
+# create data frame for results and subset the significant SNPs
+results.df <- data.frame(cbind(ld.vector, ld.fdr))
 names(results.df) <- c('r2', 'q')
 
-complete <- results.df[complete.cases(results.df),]
+# get summary stats and print results
 num.pairs <- length(results.df$r2)
-num.sig <- subset(complete, r2 > 0.8, q < 0.05)
+complete <- results.df[complete.cases(results.df),]
+num.sig <- length(subset(complete, r2 > 0.8 & q < 0.05)[,1])
 
-# does not play nice with NAs: 
-# library('fdrtool')
-# ld.fdr <- fdrtool(chisq.mat[upper.tri(chisq.mat)], statistic=c('pvalue'), plot=FALSE)
-# ld$q <- ld.fdr$qval
-# sig.ld <- subset(ld, q < 0.05 & R2 > 0.8)
-# num.sig <- length(sig.ld[,1])
+print(c(num.pairs, num.sig))
