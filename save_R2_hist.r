2d0a9e6e2cf6590914f28fa6661befe46fbd5e4b
kellyp2738 <kellyannepierc@gmail.com>
2015-08-07 17:06:48 -0600

    Modified to write histogram summary to file.

--- save_R2_hist.r
+++ save_R2_hist.r
@@ -1,22 +1,30 @@
 # create histogram of R^2 values (without plotting) and save to file
 
-args<-commandArgs()
+args<-commandArgs(trailingOnly=TRUE)
 
-infile<-args[1]
-n<-args[2]
+infile<-'plink.ld' # it will always be called this!
+n<-args[1]
+#print(n)
 
 ld.values.normal<-read.table(infile)
 ld.u<-ld.values.normal[upper.tri(ld.values.normal)]
 ld.u[!is.finite(ld.u)] <- NA
 ld.u<-ld.u[complete.cases(ld.u)]
 ld.hist<-hist(ld.u, breaks=seq(0,1,0.05), plot=F)
-
-output<-data.frame(cbind(rep(n, length(ld.hist$mids)), hist$mids, hist$density))
+output<-data.frame(cbind(rep(n, length(ld.hist$mids)), ld.hist$mids, ld.hist$density))
 names(output)<-c('n', 'mids', 'density')
 
-f.name<-file.path('~/Desktop/UT_ddRADseq/Rsq', paste(n, '.csv', sep=''))
+g.95<-length(ld.u[ld.u>0.95])/length(ld.u)
+g.90<-length(ld.u[ld.u>0.9])/length(ld.u)
+g.85<-length(ld.u[ld.u>0.85])/length(ld.u)
+g.80<-length(ld.u[ld.u>0.8])/length(ld.u)
+fs<-c(g.95, g.90, g.85, g.80)
+print(fs) # capture fraction sig in stdout/stderr to make one big file in python
 
-write.csv(output, f.name)
+f.name<-file.path('~/Desktop/UT_ddRADseq/Rsq', paste(n, 'hist.csv', sep=''))
+#g.name<-file.path('~/Desktop/UT_ddRADseq/Rsq', paste(n, 'fraction_sig.csv', sep=''))
+
+write.csv(output, f.name, row.names=F)
 
 # for when the time comes, the plotting script...
 #plot(ld.hist$mids, ld.hist$density, type='l', axes=F, xlab=expression('R'^'2'), ylab='', cex.axis=1.5, cex.lab=1.5)
