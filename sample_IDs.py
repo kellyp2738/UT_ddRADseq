d7e3503b47f39ea5a73d5c735b75870eee0de0ac
Kelly Anne Pierce <kellyannepierce@gmail.com>
2016-01-25 15:08:38 +1030

    Fixed indentation error.

--- sample_IDs.py
+++ sample_IDs.py
@@ -99,82 +99,82 @@ for n in range(min_ticks, (max_ticks+1)):
             f.writelines(id_sample[i]) # add the sample name to the file. newlines are already embedded in the name.
             f.close()
     
-            # call VCFtools with tmp.txt used as the "--keep" file
-            my_env=os.environ.copy()
-            # the arguments need to be fully separated for this to work (["--vcf", "/file/path/"], not ["--vcf /file/path/"])
-            tempPath = tempOutDir + "/vcf_tmp"
-            p = Popen(["vcftools", "--vcf", infile_VCF, "--keep", tempFile, "--min-meanDP", "20", "--minGQ", "25", "--maf", "0.05", "--max-missing", max_missing, "--recode", "--out", tempPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env)
-    
-            # extract the number of SNPs retained
-            stdout, stderr = p.communicate()
-            split_stderr = str.splitlines(stderr)
-            snp_line = split_stderr[17]
-            snp_count = [int(s) for s in snp_line.split() if s.isdigit()][0] # split snp_line, search for integers, and save the first one found
-    
-            # make a VCF file with only unique reads
-            tempVCF = tempOutDir + '/vcf_tmp.recode.vcf' # vcftools appends .recode.vcf to the file name we used above
-            tempUnique = tempOutDir + '/vcf_temp_uniqueOnly.vcf'
-            sedParseCall = "sed '/^##/ d' " + tempVCF + " | awk '!array[$1]++' > " + tempUnique
-            subprocess.call(sedParseCall, shell=True)
-    
-            # extract the number of unique reads having SNPs and save as a new VCF file
-            wcl = Popen(["wc", "-l", tempUnique], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            wcl_outs, wcl_errs = wcl.communicate()
-            unique_snp_count = str.split(wcl_outs)[0] #this will be a string
-    
-            # open a file to which sample data can be appended
-            final_data=open(outfile1, 'a')
-    
-            # save the sample size and snp count
-            write_line = [str(n), ",", str(snp_count), ",", unique_snp_count, "\n"]
-            final_data.writelines(write_line)
-            final_data.close()
-    
-            # Plink expects human data and doesn't like chromosome numbers > 22. We don't know the chromosome structure, so replace all the chromosome names with "1"
-            reParseCall = "sed '/^##/ d' " + tempVCF + " | awk '$2=NR' OFS='\t' | sed 's/1/POS/' | sed 's|_pseudoreference_pe_concatenated_without_rev_complement||g' | awk '$1=1' OFS='\t' | sed '0,/1/{s/1/#CHROM/}' > " + tempOutDir + "/reParsed.vcf"
-            subprocess.call(reParseCall, shell = True)
-            reParseCallUnique = "sed '/^##/ d' " + tempUnique + " | awk '$2=NR' OFS='\t' | sed 's/1/POS/' | sed 's|_pseudoreference_pe_concatenated_without_rev_complement||g' | awk '$1=1' OFS='\t' | sed '0,/1/{s/1/#CHROM/}' > " + tempOutDir + "/reParsedUnique.vcf"
-            subprocess.call(reParseCallUnique, shell = True)
-            
-            # get SNP IDs from vcf file
-            # convert the VCF file to a PLINK file
-            # -- full file
-            fullVCF2Plink = "vcftools --vcf " + tempOutDir + "/reParsed.vcf --plink --out " + tempOutDir + "/vcf_tmp_plink"
-            subprocess.call(fullVCF2Plink, shell=True)
-            # -- filtered file
-            filteredVCF2Plink = "vcftools --vcf " + tempOutDir + "/reParsedUnique.vcf --plink --out " + tempOutDir + "/vcf_uniqueOnly_tmp_plink" 
-            subprocess.call(filteredVCF2Plink, shell=True)
-    
-            # calculate LD on the plink file
-            # -- full file
-            fullPlink = "plink --file " + tempOutDir + "/vcf_tmp_plink --r2 --matrix --noweb"
-            subprocess.call(fullPlink, shell=True) # option A: pairwise matrix
-            
-            rr = Popen(["Rscript", "/home1/02540/kellypie/UT_ddRADseq/save_R2_hist.r", tempOutDir, str(n), '_unfiltered'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-            outs, errs = rr.communicate()
-            out_split = str.splitlines(outs) #out contains the print output from r (fraction 'significant', which in this context is simply greater than some R^2 threshold
-            print out_split 
-            outss = str.split(out_split[0])
-            outss[0] = n # replace the R print output line number, which we don't need anyway, with the sample size 
-            with open(outfile2, 'a') as c: # open in append mode
-                r2Writer = csv.writer(c, delimiter=",")
-                r2Writer.writerow(outss)
-                
-            os.remove((tempOutDir + "/plink.ld")) # delete the intermediate LD file
-                        
-            # -- filtered file
-            filteredPlink = "plink --file " + tempOutDir + "/vcf_uniqueOnly_tmp_plink --r2 --matrix --noweb"
-            subprocess.call(filteredPlink, shell=True) # option A: pairwise matrix
-            
-            rr2 = Popen(["Rscript", "/home1/02540/kellypie/UT_ddRADseq/save_R2_hist.r", str(n), '_filtered'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-            outs2, errs2 = rr2.communicate()
-            out_split2 = str.splitlines(outs2) #out contains the print output from r (fraction 'significant', which in this context is simply greater than some R^2 threshold
-            print out_split2
-            outss2 = str.split(out_split2[0])
-            outss2[0] = n # replace the R print output line number, which we don't need anyway, with the sample size    
-            with open(outfile3, 'a') as d: # open in append mode
-                r2Writer = csv.writer(d, delimiter=",")
-                r2Writer.writerow(outss2)
+		# call VCFtools with tmp.txt used as the "--keep" file
+		my_env=os.environ.copy()
+		# the arguments need to be fully separated for this to work (["--vcf", "/file/path/"], not ["--vcf /file/path/"])
+		tempPath = tempOutDir + "/vcf_tmp"
+		p = Popen(["vcftools", "--vcf", infile_VCF, "--keep", tempFile, "--min-meanDP", "20", "--minGQ", "25", "--maf", "0.05", "--max-missing", max_missing, "--recode", "--out", tempPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env)
+
+		# extract the number of SNPs retained
+		stdout, stderr = p.communicate()
+		split_stderr = str.splitlines(stderr)
+		snp_line = split_stderr[17]
+		snp_count = [int(s) for s in snp_line.split() if s.isdigit()][0] # split snp_line, search for integers, and save the first one found
+
+		# make a VCF file with only unique reads
+		tempVCF = tempOutDir + '/vcf_tmp.recode.vcf' # vcftools appends .recode.vcf to the file name we used above
+		tempUnique = tempOutDir + '/vcf_temp_uniqueOnly.vcf'
+		sedParseCall = "sed '/^##/ d' " + tempVCF + " | awk '!array[$1]++' > " + tempUnique
+		subprocess.call(sedParseCall, shell=True)
+
+		# extract the number of unique reads having SNPs and save as a new VCF file
+		wcl = Popen(["wc", "-l", tempUnique], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		wcl_outs, wcl_errs = wcl.communicate()
+		unique_snp_count = str.split(wcl_outs)[0] #this will be a string
+
+		# open a file to which sample data can be appended
+		final_data=open(outfile1, 'a')
+
+		# save the sample size and snp count
+		write_line = [str(n), ",", str(snp_count), ",", unique_snp_count, "\n"]
+		final_data.writelines(write_line)
+		final_data.close()
+
+		# Plink expects human data and doesn't like chromosome numbers > 22. We don't know the chromosome structure, so replace all the chromosome names with "1"
+		reParseCall = "sed '/^##/ d' " + tempVCF + " | awk '$2=NR' OFS='\t' | sed 's/1/POS/' | sed 's|_pseudoreference_pe_concatenated_without_rev_complement||g' | awk '$1=1' OFS='\t' | sed '0,/1/{s/1/#CHROM/}' > " + tempOutDir + "/reParsed.vcf"
+		subprocess.call(reParseCall, shell = True)
+		reParseCallUnique = "sed '/^##/ d' " + tempUnique + " | awk '$2=NR' OFS='\t' | sed 's/1/POS/' | sed 's|_pseudoreference_pe_concatenated_without_rev_complement||g' | awk '$1=1' OFS='\t' | sed '0,/1/{s/1/#CHROM/}' > " + tempOutDir + "/reParsedUnique.vcf"
+		subprocess.call(reParseCallUnique, shell = True)
+		
+		# get SNP IDs from vcf file
+		# convert the VCF file to a PLINK file
+		# -- full file
+		fullVCF2Plink = "vcftools --vcf " + tempOutDir + "/reParsed.vcf --plink --out " + tempOutDir + "/vcf_tmp_plink"
+		subprocess.call(fullVCF2Plink, shell=True)
+		# -- filtered file
+		filteredVCF2Plink = "vcftools --vcf " + tempOutDir + "/reParsedUnique.vcf --plink --out " + tempOutDir + "/vcf_uniqueOnly_tmp_plink" 
+		subprocess.call(filteredVCF2Plink, shell=True)
+
+		# calculate LD on the plink file
+		# -- full file
+		fullPlink = "plink --file " + tempOutDir + "/vcf_tmp_plink --r2 --matrix --noweb"
+		subprocess.call(fullPlink, shell=True) # option A: pairwise matrix
+		
+		rr = Popen(["Rscript", "/home1/02540/kellypie/UT_ddRADseq/save_R2_hist.r", tempOutDir, str(n), '_unfiltered'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		outs, errs = rr.communicate()
+		out_split = str.splitlines(outs) #out contains the print output from r (fraction 'significant', which in this context is simply greater than some R^2 threshold
+		print out_split 
+		outss = str.split(out_split[0])
+		outss[0] = n # replace the R print output line number, which we don't need anyway, with the sample size 
+		with open(outfile2, 'a') as c: # open in append mode
+			r2Writer = csv.writer(c, delimiter=",")
+			r2Writer.writerow(outss)
+			
+		os.remove((tempOutDir + "/plink.ld")) # delete the intermediate LD file
+					
+		# -- filtered file
+		filteredPlink = "plink --file " + tempOutDir + "/vcf_uniqueOnly_tmp_plink --r2 --matrix --noweb"
+		subprocess.call(filteredPlink, shell=True) # option A: pairwise matrix
+		
+		rr2 = Popen(["Rscript", "/home1/02540/kellypie/UT_ddRADseq/save_R2_hist.r", str(n), '_filtered'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		outs2, errs2 = rr2.communicate()
+		out_split2 = str.splitlines(outs2) #out contains the print output from r (fraction 'significant', which in this context is simply greater than some R^2 threshold
+		print out_split2
+		outss2 = str.split(out_split2[0])
+		outss2[0] = n # replace the R print output line number, which we don't need anyway, with the sample size    
+		with open(outfile3, 'a') as d: # open in append mode
+			r2Writer = csv.writer(d, delimiter=",")
+			r2Writer.writerow(outss2)
         
         #remove the temp files for that repetition
         files = glob.glob(tempOutDir)
